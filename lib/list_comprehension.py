d6554cc606ab8dc7c836e8c2765e396c31d40c03
Chief <brianmwangi10808@gmail.com>
2024-04-12 14:40:25 +0300

    commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/env python3
+
 def return_evens(num_list):
-    pass
+    '''Returns a list containing only the even numbers from num_list.'''
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    '''Adds an exclamation mark to the end of each sentence in sentence_list.'''
+    return [sentence + "!" for sentence in sentence_list]
+
+if __name__ == "__main__":
+    num_list = range(10)
+    print(return_evens(num_list))
+
+    sentence_list = [
+        "I like computers",
+        "I require coffee",
+        "Live long and prosper",
+    ]
+    print(make_exclamation(sentence_list))
